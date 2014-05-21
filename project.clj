76550a77a3819b1c2c72d8705a04175cdab8537d
Darrell Hamilton <darrell.noice@gmail.com>
2014-05-20 19:00:21 -0600

    developers pom addition

--- project.clj
+++ project.clj
@@ -3,4 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :pom-addition [:developers [:developer
+                              [:id "zeroem"]
+                              [:name "Darrell Hamilton"]
+                              [:url "https://github.com/zeroem"]]])
